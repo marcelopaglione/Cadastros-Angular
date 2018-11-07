10749db0635e56456403d1dfeeeab0f4e12fdce0
Marcelo <m.paglionejr@gmail.com>
2018-11-07 21:29:45 -0200

    Added Blank person module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PersonFormComponent } from './person-form/person-form.component';
+import { PersonModuleModule } from './person-form/person.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonFormComponent
   ],
   imports: [
     BrowserModule,
+    PersonModuleModule,
     AppRoutingModule
   ],
   providers: [],
