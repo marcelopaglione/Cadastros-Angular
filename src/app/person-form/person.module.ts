34ae59629c8edb87cb97f9f14be6e9c46d3981aa
Marcelo <m.paglionejr@gmail.com>
2018-11-07 23:56:29 -0200

    added debug module

--- src/app/person-form/person.module.ts
+++ src/app/person-form/person.module.ts
@@ -1,11 +1,15 @@
+
+
 import { PersonFormComponent } from './person-form.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { DebugComponent } from '../debug/debug.component';
 
 @NgModule({
   declarations: [
-    PersonFormComponent
+    PersonFormComponent,
+    DebugComponent
   ],
   imports: [
     CommonModule,
